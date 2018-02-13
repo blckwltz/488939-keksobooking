02fa38377b920f5229f5514f9e54c95ff60d6f44
blckwltz <ruslanbreev@gmail.com>
2018-02-13 16:18:19 +0300

    Выполнил задание

---
+++ js/data.js
@@ -0,0 +1,112 @@
+'use strict';
+
+(function () {
+  var ADVERTS_NUMBER = 8;
+
+  var PRICE_RANGE = {
+    min: 1000,
+    max: 1000000
+  };
+
+  var ROOMS_NUMBER = {
+    min: 1,
+    max: 5
+  };
+
+  var GUESTS_NUMBER = {
+    min: 1,
+    max: 7
+  };
+
+  var MAP_AREA = {
+    x: {min: 300, max: 900},
+    y: {min: 150, max: 500}
+  };
+
+  var getRandomNumber = function (min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  var getRandomElement = function (array) {
+    return array[getRandomNumber(0, array.length)];
+  };
+
+  var randomSeed = function () {
+    return Math.random() - 0.5;
+  };
+
+  var titles = [
+    'Большая уютная квартира',
+    'Маленькая неуютная квартира',
+    'Огромный прекрасный дворец',
+    'Маленький ужасный дворец',
+    'Красивый гостевой домик',
+    'Некрасивый негостеприимный домик',
+    'Уютное бунгало далеко от моря',
+    'Неуютное бунгало по колено в воде'
+  ];
+
+  var type = [
+    'flat',
+    'house',
+    'bungalo'
+  ];
+
+  var times = [
+    '12:00',
+    '13:00',
+    '14:00'
+  ];
+
+  var features = [
+    'wifi',
+    'dishwasher',
+    'parking',
+    'washer',
+    'elevator',
+    'conditioner'
+  ];
+
+  features.length = getRandomNumber(0, 6);
+
+  var photos = [
+    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+  ];
+
+  var sortedPhotos = photos.sort(randomSeed);
+
+  var adverts = [];
+
+  for (var i = 0; i < ADVERTS_NUMBER; i++) {
+    var point = {
+      x: getRandomNumber(MAP_AREA.x.min, MAP_AREA.x.max),
+      y: getRandomNumber(MAP_AREA.y.min, MAP_AREA.y.max)
+    };
+    adverts.push({
+      author: {
+        avatar: 'img/avatars/user0' + (i + 1) + '.png'
+      },
+      offer: {
+        title: titles[i],
+        address: point.x + ', ' + point.y,
+        price: getRandomNumber(PRICE_RANGE.min, PRICE_RANGE.max),
+        type: getRandomElement(type),
+        rooms: getRandomNumber(ROOMS_NUMBER.min, ROOMS_NUMBER.max),
+        guests: getRandomNumber(GUESTS_NUMBER.min, GUESTS_NUMBER.max),
+        checkin: getRandomElement(times),
+        checkout: getRandomElement(times),
+        features: features,
+        description: '',
+        photos: sortedPhotos
+      },
+      location: {
+        x: point.x,
+        y: point.y
+      }
+    });
+  }
+
+  window.adverts = adverts;
+})();
---
+++ js/form.js
@@ -0,0 +1,92 @@
+'use strict';
+
+(function () {
+  var noticeForm = document.querySelector('.notice__form');
+
+  noticeForm.action = 'https://js.dump.academy/keksobooking';
+
+  var noticeFormTitle = noticeForm.querySelector('#title');
+
+  noticeFormTitle.required = true;
+
+  noticeFormTitle.minLength = 30;
+
+  noticeFormTitle.maxLength = 100;
+
+  var noticeFormAddress = noticeForm.querySelector('#address');
+
+  noticeFormAddress.disabled = true;
+
+  var noticeFormPrice = noticeForm.querySelector('#price');
+
+  noticeFormPrice.required = true;
+
+  noticeFormPrice.min = 1000;
+
+  noticeFormPrice.max = 1000000;
+
+  var noticeFormType = noticeForm.querySelector('#type');
+
+
+  var onTypeChange = function () {
+    if (noticeFormType.value === 'bungalo') {
+      noticeFormPrice.min = 0;
+    } if (noticeFormType.value === 'flat') {
+      noticeFormPrice.min = 1000;
+    } if (noticeFormType.value === 'house') {
+      noticeFormPrice.min = 5000;
+    } if (noticeFormType.value === 'palace') {
+      noticeFormPrice.min = 10000;
+    }
+  };
+
+  noticeFormType.addEventListener('input', function () {
+    onTypeChange();
+  });
+
+  var noticeFormTimeIn = noticeForm.querySelector('#timein');
+
+  var noticeFormTimeOut = noticeForm.querySelector('#timeout');
+
+  var onTimeChange = function (left, right) {
+    right.value = left.value;
+  };
+
+  noticeFormTimeIn.addEventListener('input', function () {
+    onTimeChange(noticeFormTimeIn, noticeFormTimeOut);
+  });
+
+  noticeFormTimeOut.addEventListener('input', function () {
+    onTimeChange(noticeFormTimeOut, noticeFormTimeIn);
+  });
+
+  var noticeFormRoomNumber = noticeForm.querySelector('#room_number');
+
+  var noticeFormCapacity = noticeForm.querySelector('#capacity');
+
+  var options = [
+    [2],
+    [1, 2],
+    [0, 1, 2],
+    [3]
+  ];
+
+  var onRoomsChange = function () {
+    var allowedOptions = options[noticeFormRoomNumber.selectedIndex];
+    var defaultOption = allowedOptions[0];
+    noticeFormCapacity[defaultOption].selected = true;
+    [].forEach.call(noticeFormCapacity.options, function (option, index) {
+      option.hidden = !allowedOptions.includes(index);
+    });
+  };
+
+  noticeFormRoomNumber.addEventListener('input', onRoomsChange);
+
+  var noticeFormSubmit = noticeForm.querySelector('.form__submit');
+
+  noticeFormSubmit.addEventListener('click', function (evt) {
+    if (noticeFormCapacity.value > noticeFormRoomNumber.value) {
+      evt.preventDefault();
+    }
+  });
+})();
